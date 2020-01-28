e3387e116f9cb449dab3ec0321a88bfa4192997e
Gintama7 <43084982+Gintama7@users.noreply.github.com>
2020-01-28 13:38:42 +0530

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,8 @@
+a=int(input("enter a number to see if it is odd or even"))
+if a%2==0:
+    print("Number is even")
+elif a%2!=0:
+    print("Number is odd")
+else :
+    print("Number is invalid")
+
---
+++ pytha.py
@@ -0,0 +1,4 @@
+import math
+b=int(input("Enter base"))
+p=int(input("Enter height"))
+h=print(math.sqrt(math.pow(b,2)+math.pow(p,2)))
